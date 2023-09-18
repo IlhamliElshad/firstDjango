dad122f743e97f6f96e2f51d50cebcc562e2229f
Elshad <ilhamli456@gmail.com>
2023-09-18 20:35:17 +0400

    http response evezine html return

--- project/books/views.py
+++ project/books/views.py
@@ -3,4 +3,11 @@ from django.shortcuts import render,HttpResponse
 # Create your views here.
 
 def book(request):
-    return HttpResponse("salam necesiniz?")
+    context = {
+        "user1": "imran",
+        "user2": "elshad",
+        "user3": "orxan",
+        "user4": "ilkin"
+        
+    }
+    return render(request,"index.html",context)
